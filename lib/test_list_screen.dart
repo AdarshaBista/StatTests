42f24e341e9560c2a06e2d39e0f5ce16560bc845
AdarshaBista <adrsh07@gmail.com>
2019-02-10 09:32:42 +0545

    Switched to tabbed layout on test list screen

--- lib/test_list_screen.dart
+++ lib/test_list_screen.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
-import './widgets/test_list_card.dart';
 import './widgets/custom_appbar.dart';
 import './gap_test/gap_input_screen.dart';
 import './auto_correlation_test/ac_input_screen.dart';
 
-class TestListScreen extends StatelessWidget {
+class TestListScreen extends StatefulWidget {
+  @override
+  TestListScreenState createState() {
+    return new TestListScreenState();
+  }
+}
+
+class TestListScreenState extends State<TestListScreen>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+  int currentPage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = new TabController(
+      length: 5,
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,45 +37,26 @@ class TestListScreen extends StatelessWidget {
         context: context,
         title: "Choose a Test",
       ),
-      body: ListView(
-        shrinkWrap: true,
-        itemExtent: 100.0,
-        physics: BouncingScrollPhysics(),
+      bottomNavigationBar: Material(
+        color: Theme.of(context).primaryColor,
+        child: TabBar(
+          indicatorSize: TabBarIndicatorSize.tab,
+          controller: _tabController,
+          tabs: <Widget>[
+            Tab(
+              text: "GT",
+            ),
+            Tab(
+              text: "ACT",
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
         children: <Widget>[
-          TestListCard(
-            title: "Chi Square Test",
-            bgPath: "",
-            onPressed: null,
-          ),
-          TestListCard(
-            title: "KS Test",
-            bgPath: "",
-            onPressed: null,
-          ),
-          TestListCard(
-            title: "Gap Test",
-            bgPath: "",
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => GapInputScreen(),
-                ),
-              );
-            },
-          ),
-          TestListCard(
-            title: "Auto Correlation Test",
-            bgPath: "",
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => ACInputScreen(),
-                ),
-              );
-            },
-          ),
+          GapInputScreen(),
+          ACInputScreen(),
         ],
       ),
     );
